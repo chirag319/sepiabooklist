7a02f57fa4d5b3cb356406a683e1de539b47f6b3
chirag <mca.chirag@yahoo.com>
2023-02-06 15:16:22 +0530

    Initial Commit for sepia book list

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sepiabooklist/core/main.dart';
+import 'package:sepiabooklist/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
